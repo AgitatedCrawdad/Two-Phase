cfb8601be1ae3c4d2dd01c1b737a1283af08182c
AgitatedCrawdad <rocketpower444444@yahoo.com>
2018-09-04 16:09:49 -0500

    Cleaned
    Removed unnecessary lines

--- VoidQuality.m
+++ VoidQuality.m
@@ -1,25 +1,27 @@
 %% Plot of the flow quality
-f=figure(2); %Calculate void fraction from pressure drop
-% trialname = 'Trial_60C_2_tapwater2'
+trialname = 'Trial_60C_2_tapwater2'
 % trialname = 'Trial_60C_points_DI_2'
-trialname = 'Trial_60C_2g'
-% [x1,p1,p1err]=Plot2(trialname,'P1');
-% [x1,t5,t5err]=Plot2(trialname,'T5');
-% [x1,m,merr]=Plot2(trialname,'M');
-% [x1,p3,p3err]=Plot2(trialname,'P3');
-
+% trialname = 'Trial_60C_2g'
+figure(1)
+[x1,p1,p1err]=Plot2(trialname,'P1');
+[x1,t5,t5err]=Plot2(trialname,'T5');
+[x1,m,merr]=Plot2(trialname,'M');
+[x1,p3,p3err]=Plot2(trialname,'P3');
+% 
 % [x1,p1,p1err]=Plot(trialname,'P1');
 % hold on
-% % [x1,t5,t5err]=Plot(trialname,'T5');
-% % [x1,m,merr]=Plot(trialname,'M');
+% [x1,t5,t5err]=Plot2('Trial_60C_2_tapwater2','T5');
+% [x1,m,merr]=Plot(trialname,'M');
 % [x1,p3,p3err]=Plot(trialname,'P3');
-close all
+close gcf
+
+% figure(4)
 
 Tin = t5; %C
 q = x1./1000; %kW
 % p3top1dist = linspace(0,1.47,200); %m
-diameter = 0.015875; %m heater diameter
-heatercircum = pi*diameter;%m
+heaterdiameter = 0.015875; %m heater diameter
+heatercircum = pi*heaterdiameter;%m
 heaterlength = 0.6; %m
 heatflux = q/(heatercircum*heaterlength); %kW/m^2
 x = linspace(0,heaterlength,100);
@@ -28,7 +30,7 @@ cp = 4.186; %kJ/kgK
 
 Tout = zeros(length(x),length(m_dot)); %100x8
 for i=1:length(Tin)
-    Tout(:,i) = Tin(i) + (heatflux(i)*(pi*diameter*x))/(m_dot(i)*cp);
+    Tout(:,i) = Tin(i) + (heatflux(i)*(pi*heaterdiameter*x))/(m_dot(i)*cp);
 end
 
 deltap = p1-p3;
@@ -65,39 +67,21 @@ for i =1:290
     alpha(100+i,:) = alpha(100,:); 
 end
 avgalpha = sum(alpha)/length(alpha);
-% plot(x1,avgalpha)
-% 
-% figure(1)
-% grid on
-% yyaxis left
-% plot(x,Tout)
-% % figure(2)
-% yyaxis right
-% plot(x1,avgalpha)
 
-f=figure(2); %Calculate void fraction from pressure drop
-% Plot2('Trial_60C_1g','P1')
-% trialname = 'Trial_60C_2_tapwater2'
+dp = p1-p3;
 
-Plot2(trialname,'P1');
-xdata1=f.Children.Children.XData;
-ydata1=f.Children.Children.YData;
-hold on
-Plot2(trialname,'P3');
-
-% Plot2('Trial_60C_1g','P3')
-xdata2=f.Children.Children.XData;
-ydata2=f.Children.Children.YData;
-clf
-% close all
-
-dp = ydata1-ydata2;
-rhoH = ((dp-0.3)*6894.76)/(9.81*1.4478); %Calculates homogeneous density using Pstatic = rho_Homo*g*H*sin(theta)
+rhoH = ((dp)*6894.76)/(9.81*1.4478); %Calculates homogeneous density using Pstatic = rho_Homo*g*H*sin(theta)
 
 voidfraction = (rhoH-density_liq)/(density_vap-density_liq);
-plot(x1,avgalpha);
-hold on
-plot(xdata1,voidfraction);
+% plot(x1,avgalpha);
+% hold on
+% plot(x1,voidfraction)
+% grid on
+% ylabel('Void Fraction [\alpha]')
+% xlabel('Power [Watts]')
+% legend('Method 1 - Sat. P & T','Method 2 - Pressure Drop')
 
-legend('Method 1 - Sat. P & T','Method 2 - Pressure Drop')
+channeldiameter = 0.0254; %m
+channelarea = pi*(channeldiameter/2)^2;
+massflux = m/channelarea;
 
